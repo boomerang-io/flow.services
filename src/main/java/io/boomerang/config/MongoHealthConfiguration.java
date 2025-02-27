b7f2354f9662906fbf322977141115a07a810d3f
Tyson Lawrie <tlawrie@users.noreply.github.com>
2025-02-27 21:57:18 +1100

    fix: remove the mongodb healthcheck hello issue

--- src/main/java/io/boomerang/config/MongoHealthConfiguration.java
+++ src/main/java/io/boomerang/config/MongoHealthConfiguration.java
@@ -1,12 +1,17 @@
- package io.boomerang.config;
+package io.boomerang.config;
 
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
+/**
+ * Custom health indicator for MongoDB to override out of the box health check. *
+ *
+ * <p>management.health.mongo.enabled=false
+ */
 @Component
- public class MongoHealthConfiguration implements HealthIndicator {
+public class MongoHealthConfiguration implements HealthIndicator {
 
   private final MongoTemplate mongoTemplate;
 
@@ -23,4 +28,4 @@ import org.springframework.stereotype.Component;
       return Health.down(e).build();
     }
   }
- }
+}
